569e750f98dac54b0353f27800981d6e13ec561f
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-04-20 22:23:46 +0500

    Product Model

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
-import email
+
+from email.mime import image
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -12,4 +13,10 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False,null=True,blank=True)
 
+    def __str__(self):
+        return self.name
