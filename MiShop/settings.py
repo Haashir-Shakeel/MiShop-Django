1fe25faf5e2ab6fa874aa9bd52f040ca77160221
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-04-21 06:24:11 +0500

    setting MEDIA_ROOT and MEDIA_URL in settings.py

--- MiShop/settings.py
+++ MiShop/settings.py
@@ -126,6 +126,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR , 'static')
 ]
 
+MEDIA_URL = '/images/products/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/products')
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
