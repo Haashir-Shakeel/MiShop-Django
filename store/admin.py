1895320146102493b5667c8d4e38cc7c91f2086d
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-04-21 05:26:16 +0500

    some changes in store.models and registering models in admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from store.models import Customer,Order,OrderItem,ShippingAddress,Product
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+admin.site.register(Product)
+
+
