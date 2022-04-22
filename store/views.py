e4702e3ef3b3fe2bca2066dbdc0720b3837e29d5
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-04-22 06:20:50 +0500

    rendering data in cart page

--- store/views.py
+++ store/views.py
@@ -10,7 +10,7 @@ def store(request):
     return render(request,'store/store.html',context)
 
 def cart(request):
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer,complete=False)
         items = order.orderitem_set.all()
