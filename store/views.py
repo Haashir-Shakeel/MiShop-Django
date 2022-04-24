6a8adeef20c08e638df4295db9e50755c75b10cc
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-04-24 08:19:36 +0500

    add to cart logic and working

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
+
 from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
+import json
 # Create your views here.
 
 def store(request):
@@ -40,4 +42,25 @@ def checkout(request):
 
 
 def updateItem(request):
-    return JsonResponse("Item was added",safe=False)
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action: ', action)
+    print('Product: ',productId)
+
+    customer  = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer,complete=False)
+    orderItem, created = OrderItem.objects.get_or_create(order=order,product=product)
+
+    if action == 'add':
+        orderItem.quantity +=1
+    elif action == 'remove':
+        orderItem.quantity -=1
+
+    orderItem.save()
+
+    if orderItem.quantity <=0:
+        orderItem.delete() 
+
+    return JsonResponse('Item was Added',safe=False)
