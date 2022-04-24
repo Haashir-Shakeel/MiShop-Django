0124d1400f6dccd3b6587e821ae9f83c9f7373b1
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-04-24 09:04:57 +0500

    getting cart quantity in template

--- store/views.py
+++ store/views.py
@@ -6,9 +6,20 @@ import json
 # Create your views here.
 
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items=[]
+        order = {'get_cart_total':0,'get_cart_items':0}
+        cartItems = order['get_cart_items']
+
     products = Product.objects.all()
     context = {
         'products':products,
+        'cartItems':cartItems,
     }
     return render(request,'store/store.html',context)
 
@@ -17,12 +28,15 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer,complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items=[]
         order = {'get_cart_total':0,'get_cart_items':0}
+        cartItems = order['get_cart_items']
     context = {
         'items':items,
         'order': order,
+        'cartItems':cartItems,
     }
     return render(request,'store/cart.html',context)
 
@@ -31,12 +45,15 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer,complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items=[]
         order = {'get_cart_total':0,'get_cart_items':0}
+        cartItems = order['get_cart_items']
     context = {
         'items':items,
         'order': order,
+        'cartItems':cartItems,
     }
     return render(request,'store/checkout.html',context)
 
